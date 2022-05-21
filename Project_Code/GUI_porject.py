198566353d1e2834d2fd5174b9709efd03a2e2cb
Erik <erikarancibia2108@gmail.com>
2022-05-21 18:23:46 +0600

    Second Commit Second Commit

--- Project_Code/GUI_porject.py
+++ Project_Code/GUI_porject.py
@@ -11,7 +11,17 @@ root.title("VisualAP") #The name of it
 
 root.iconbitmap("C:/Users/Erik/Desktop/GUI_Final_Project/Icon/Logo.ico") #This is the icon of alatoo universtity
 
-button1 = tk.Button(root, text='Press Me')
-button1.pack()
+l=tk.Label(root, text='pictures will show in this spot', image=None) #Create a label 
+l.pack() #The executor of the label
+
+def openpicture():
+	global img 
+	filename = filedialog.askopenfilename() #Obtain the compleate rout 
+	img = ImageTk.PhotoImage(Image.open(filename)) #tkinter only can open gif files, here I used the libary PIL 
+	#Open the file in jpg Format
+	l.config(image = img) #I used the method of config to put the image in th label
+
+b = tk.Button(root, text='select a picture', command=openpicture) #I configure the boton and the command open picture
+b.pack() 
 
 tk.mainloop()
